7765c6cc420b0b0f18cd9f1da61d279527f0f452
Suneel F M <Smavinagidad@tandemdiabetes.com>
2025-11-17 16:50:15 +0530

    Created select field

---
+++ src/components/atoms/SelectFiled.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../../styles/Atoms.module.css";
+
+type SelectFiledProps = {
+  label: string;
+  options: any[];
+};
+export default function SelectFiled(props: SelectFiledProps) {
+  const { label, options } = props;
+  return (
+    <select className={styles.field}>
+      <option value={label}>{label}</option>
+      {options.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+}
